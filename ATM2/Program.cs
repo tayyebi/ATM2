4ecbfb90d1be1d00c3e11115ce95fa72631812e9
tayyebi <tayyebimohammadreza@gmail.com>
2017-07-16 08:13:22 +0430

    Update some data

--- ATM2/Program.cs
+++ ATM2/Program.cs
@@ -12,9 +12,46 @@ namespace ATM2
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        // [STAThread]
         static void Main()
         {
+
+            // Data Generate
+
+            //List<int> transactionIds;
+            //List<string> atmCodes;
+            //List<int> carIds;
+
+            //using (var db = new Models.MainModel())
+            //{
+            //    transactionIds = db.Transactions.Select(x => x.Id).ToList();
+            //    atmCodes = db.ATMs.Select(x => x.Code).ToList();
+            //    carIds = db.Cars.Select(x => x.Id).ToList();
+            //}
+            //Random r = new Random();
+            //Parallel.For(0, transactionIds.Count(), j =>
+            //{
+            //    using (var db = new Models.MainModel())
+            //    {
+            //        bool atm = Convert.ToBoolean(r.Next(0, 10) > 3);
+            //        var this_tran = db.Transactions.Find(transactionIds[j]);
+            //        if (this_tran.AtmId == null && this_tran.CarId == null)
+            //        {
+            //            if (atm)
+            //            {
+            //                string code = atmCodes[r.Next(0, atmCodes.Count - 1)];
+            //                this_tran.AtmId = code;
+            //            }
+            //            else
+            //            {
+            //                int id = carIds[r.Next(0, carIds.Count - 1)];
+            //                this_tran.CarId = id;
+            //            }
+            //            /* db.SaveChangesAsync(); */
+            //            db.SaveChanges();
+            //        }
+            //    }
+            //});
 
             Application.ThreadException += (sender, args) => RexaAppException(sender, args.Exception);
             AppDomain.CurrentDomain.UnhandledException += (sender, args) => RexaAppException(sender, args.ExceptionObject as Exception);
